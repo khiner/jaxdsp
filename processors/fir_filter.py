f24f9138bea1f31de4931a524d90e5ae532f9130
Karl Hiner <karl.hiner@gmail.com>
2020-11-15 14:24:29 -0800

    Initialize processor params with a no-op prior

--- processors/fir_filter.py
+++ processors/fir_filter.py
@@ -1,5 +1,12 @@
 import jax.numpy as jnp
 
+def init_params(shape):
+    size = shape[1]
+    init = jnp.zeros(shape)
+    if size >= 1:
+        init = init.at[:,0].set(1.0)
+    return init
+
 def init_state_from_params(params):
     B, = params
     inputs = jnp.zeros(B.size)
