002b4f50b2dcb957d1723edae6c1976f860d7080
Karl Hiner <karl.hiner@gmail.com>
2020-11-23 22:34:16 -0800

    cleanup

--- loss_fns.py
+++ loss_fns.py
@@ -1,20 +1,7 @@
 from jax import jit
-import sys
-sys.path.append('./')
-sys.path.append('./processors')
-from serial_processors import SerialProcessors
-from process import process, process_serial
 
 def mse(X, Y):
     return ((Y - X) ** 2).mean()
 
 def mae(X, Y):
     return jnp.abs(Y - X).mean()
-
-def processor_loss(params, processor_class, X, Y_target):
-    Y = process(params, processor_class, X)
-    return mse(Y, Y_target)
-
-def processor_loss_serial(params, processor_class, processor_classes, X, Y_target):
-    Y = process_serial(params, processor_class, processor_classes, X)
-    return mse(Y, Y_target)
--- train.py
+++ train.py
@@ -9,18 +9,21 @@ sys.path.append('./')
 sys.path.append('./processors')
 from serial_processors import SerialProcessors
 from process import process, process_serial
-from loss_fns import processor_loss, processor_loss_serial
+from loss_fns import mse
 
 def train(processor_class, X, step_size=0.1, num_batches=100):
     params_target = processor_class.create_params_target()
     params_history = {key: [param] for (key, param) in processor_class.init_params().items()}
     loss_history = np.zeros(num_batches)
     Y_target = process(params_target, processor_class, X)
-    processor_class_loss = partial(processor_loss, processor_class=processor_class, X=X, Y_target=Y_target)
+
+    def processor_loss(params):
+        Y = process(params, processor_class, X)
+        return mse(Y, Y_target)
 
     opt_init, opt_update, get_params = optimizers.adam(step_size)
     opt_state = opt_init(processor_class.init_params())
-    grad_fn = jit(value_and_grad(processor_class_loss))
+    grad_fn = jit(value_and_grad(processor_loss))
     for batch_i in range(num_batches):
         loss, gradient = grad_fn(get_params(opt_state))
         opt_state = opt_update(batch_i, gradient, opt_state)
@@ -40,8 +43,12 @@ def train_serial(processor_classes, X, step_size=0.1, num_batches=100):
     params_history = {processor_key: {key: [param] for (key, param) in inner_init_params.items()} for (processor_key, inner_init_params) in processor_class.init_params(processor_classes).items()}
     loss_history = np.zeros(num_batches)
     Y_target = process_serial(params_target, processor_class, processor_classes, X)
-    processor_class_loss_serial = partial(processor_loss_serial, processor_classes=processor_classes, processor_class=processor_class, X=X, Y_target=Y_target)
-    grad_fn = jit(value_and_grad(processor_class_loss_serial))
+
+    def processor_loss_serial(params):
+        Y = process_serial(params, processor_class, processor_classes, X)
+        return mse(Y, Y_target)
+
+    grad_fn = jit(value_and_grad(processor_loss_serial))
 
     opt_init, opt_update, get_params = optimizers.adam(step_size)
     opt_state = opt_init(processor_class.init_params(processor_classes))
