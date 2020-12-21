ad788c7275eb038573bfb56acd61bd6bdc025cfe
Karl Hiner <karl.hiner@gmail.com>
2020-12-21 12:59:08 -0800

    Use `jax.vmap` to vectorize loss function across minibatches

--- train.py
+++ train.py
@@ -2,13 +2,19 @@ import numpy as np
 import jax.numpy as jnp
 from jax import grad, value_and_grad, jit, vmap
 from jax.experimental import optimizers
-from functools import partial
+import collections.abc
 
 import sys
 sys.path.append('./')
 sys.path.append('./processors')
 import serial_processors
 from loss_fns import mse
+
+def map_nested_dicts(ob, func):
+    if isinstance(ob, collections.abc.Mapping):
+        return {k: map_nested_dicts(v, func) for k, v in ob.items()}
+    else:
+        return func(ob)
 
 def process(processor, params, X, *init_state_args):
     return processor.tick_buffer({'params': params, 'state': processor.init_state(*init_state_args)}, X)
@@ -18,26 +24,26 @@ def evaluate(params_estimated, params_target, processor, X, *init_state_args):
     Y_target = process(processor, params_target, X, *init_state_args)
     return X, Y_estimated, Y_target
 
-def train(processors, Xs, step_size=0.1, num_batches=100):
+def train(processors, Xs, step_size=0.05, num_batches=200, batch_size=32):
     processor = serial_processors
     params_target = processor.create_params_target(processors)
     params_history = {processor_key: {key: [param] for (key, param) in inner_init_params.items()} for (processor_key, inner_init_params) in processor.init_params(processors).items()}
     loss_history = np.zeros(num_batches)
 
-    # TODO use [vmap](https://jax.readthedocs.io/en/latest/jax.html#jax.vmap)
-    #   to efficiently map this loss function across batches
     def processor_loss(params, X):
         Y_target = process(processor, params_target, X, processors)
         Y = process(processor, params, X, processors)
         return mse(Y, Y_target)
 
-    grad_fn = jit(value_and_grad(processor_loss))
-
-    opt_init, opt_update, get_params = optimizers.adam(step_size)
+    grad_fn = jit(vmap(value_and_grad(processor_loss), in_axes=(None, 0), out_axes=0))
+    opt_init, opt_update, get_params = optimizers.sgd(step_size)
     opt_state = opt_init(processor.init_params(processors))
     for batch_i in range(num_batches):
-        loss, gradient = grad_fn(get_params(opt_state), Xs[batch_i % Xs.shape[0]])
-        opt_state = opt_update(batch_i, gradient, opt_state)
+        Xs_batch = Xs[np.random.choice(Xs.shape[0], size=batch_size)]
+        loss, grads = grad_fn(get_params(opt_state), Xs_batch)
+        grads = map_nested_dicts(grads, lambda grad: np.mean(grad, axis=0))
+        loss = np.mean(loss)
+        opt_state = opt_update(batch_i, grads, opt_state)
         loss_history[batch_i] = loss
         new_params = get_params(opt_state)
         for (processor_key, processor_params) in new_params.items():
