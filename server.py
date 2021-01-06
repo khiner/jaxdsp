8c3c44d04d6ce7d17c81839d1e7cd13e3569d363
Karl Hiner <karl.hiner@gmail.com>
2021-01-05 17:52:12 -0800

    Remove audio file recorder

--- server.py
+++ server.py
@@ -12,13 +12,12 @@ import aiohttp_cors
 from av import AudioFrame, VideoFrame
 
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer
 
 ROOT = os.path.dirname(__file__)
 
 logger = logging.getLogger("pc")
 pcs = set()
-
 
 class VideoTransformTrack(MediaStreamTrack):
     """
@@ -115,10 +114,6 @@ async def offer(request):
 
     # prepare local media
     player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
-    if args.write_audio:
-        recorder = MediaRecorder(args.write_audio)
-    else:
-        recorder = MediaBlackhole()
 
     @pc.on("datachannel")
     def on_datachannel(channel):
@@ -140,7 +135,6 @@ async def offer(request):
 
         if track.kind == "audio":
             pc.addTrack(player.audio)
-            recorder.addTrack(track)
         elif track.kind == "video":
             local_video = VideoTransformTrack(
                 track, transform=params["video_transform"]
@@ -150,11 +144,9 @@ async def offer(request):
         @track.on("ended")
         async def on_ended():
             log_info("Track %s ended", track.kind)
-            await recorder.stop()
 
     # handle offer
     await pc.setRemoteDescription(offer)
-    await recorder.start()
 
     # send answer
     answer = await pc.createAnswer()
@@ -185,7 +177,6 @@ if __name__ == "__main__":
         "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
     )
     parser.add_argument("--verbose", "-v", action="count")
-    parser.add_argument("--write-audio", help="Write received audio to a file")
     args = parser.parse_args()
 
     if args.verbose:
