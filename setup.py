f0be5ec463632f6281b6af2cf7453d31e8c5ad8e
Karl Hiner <karl.hiner@gmail.com>
2021-04-23 13:31:31 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jaxdsp",
-    version="0.1.0",
+    version="0.1.1",
     author="Karl Hiner",
     author_email="karl.hiner@gmail.com",
     description="Fast, differentiable audio processors on the CPU or GPU",
